e18573be2d7626f89919bde75ebd2c227343f099
lcm40mill <lcm40mill@gmail.com>
2025-10-09 16:35:43 -0500

    Added run_python_function ability

---
+++ functions/run_python_file.py
@@ -0,0 +1,41 @@
+import os, subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    abs_path_work_dir = os.path.abspath(working_directory)
+    abs_path_file = os. path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_path_file.startswith(abs_path_work_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.exists(abs_path_file):
+        return f'Error: File "{file_path}" not found.'
+    
+    if not abs_path_file.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    
+    try:
+        command = ["python", abs_path_file]
+        if args:
+            command.extend(args)
+        
+        result = subprocess.run(
+            command,
+            capture_output=True,
+            text= True,
+            timeout=30,
+            cwd = abs_path_work_dir
+            )
+        output = []
+        
+        if result.stdout:
+            output.append(f'STDOUT: \n{result.stdout}')
+        if result.stderr:
+            output.append(f'STDERR: \n{result.stderr}')
+        if result.returncode != 0:
+            output.append(f'Process exited with code \n{result.returncode}')
+        
+        return "\n".join(output) if output else "No output produced."
+
+    except Exception as e:
+        raise f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,17 +1,24 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 
 def tests():
-    test1 = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print(test1)
+    test = run_python_file("calculator", "main.py")
+    print(test)
 
-    test2= write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print(test2)
+    test = run_python_file("calculator", "main.py", ["3 + 5"])
+    print(test)
 
-    test3 = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print(test3)
+    test = run_python_file("calculator", "tests.py")
+    print(test)
+
+    test = run_python_file("calculator", "../main.py")
+    print(test)
+
+    test = run_python_file ("calculator", "nonexistent.py")
+    print(test)
 
 if __name__ == "__main__":
     tests()
